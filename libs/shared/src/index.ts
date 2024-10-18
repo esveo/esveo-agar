89709b0692a273326354eb670ac84c90e9407871
Paul Dittrich <paul.dittrich@esvo.com>
2024-10-18 10:00:18 +0000

    fix

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -46,11 +46,6 @@ export type ClientRespawnEvent = {
   type: "respawn";
 };
 
-export type ServerWelcomeEvent = {
-  playerId: PlayerId;
-  mapSize: Vector;
-};
-
 export type ClientEvent = ClientInputEvent | ClientRespawnEvent;
 
 export type ServerGameStateUpdateEvent = {
