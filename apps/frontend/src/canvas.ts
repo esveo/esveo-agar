d3c0e2715d7f81185e77dbe3a3905accf191a6b8
PhilippDehler <philipp.dehler@esveo.com>
2024-10-18 10:59:04 +0200

    add auto resize

--- apps/frontend/src/canvas.ts
+++ apps/frontend/src/canvas.ts
@@ -1,15 +1,23 @@
+import { Vector } from "@esveo-agar/shared";
 import { WebsocketClient } from "./startWebSocketClient.ts";
 
 export function initCanvas(client: WebsocketClient) {
   const canvas = document.getElementById("game") as HTMLCanvasElement;
 
+  const center: Vector = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+  };
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  document.addEventListener("resize", () => {
+  window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    center.x = window.innerWidth / 2;
+    center.y = window.innerHeight / 2;
   });
+
   document.onpointerenter = (event) => {};
   document.onpointerdown = (event) => {};
   document.onpointermove = (event) => {};
