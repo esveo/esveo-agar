0fb7376e97f044299e8eeabf0bd2d3e3d0fb2cc8
Paul Dittrich <paul.dittrich@esvo.com>
2024-10-18 08:21:38 +0000

    define types

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,1 +1,53 @@
-export const sharedTestString = "Shared test string";
+export type Vector = {
+  x: number;
+  y: number;
+};
+
+type PlayerId = number;
+
+export type Player = {
+  id: PlayerId;
+  position: Vector;
+  /**
+   * Radius of zero means dead
+   */
+  radius: number;
+};
+
+export type Particle = Vector;
+
+export type GameState = {
+  players: Record<PlayerId, Player>;
+  particles: Particle[];
+};
+
+// TODO: move to server
+export type ClientInputState = {
+  /**
+   * The normalized direction the player is moving in
+   */
+  direction: Vector;
+};
+
+export type ClientInputEvent = {
+  playerId: PlayerId;
+  type: "input";
+  /**
+   * Normalized
+   */
+  direction: Vector;
+};
+
+export type ClientRespawnEvent = {
+  playerId: PlayerId;
+  type: "respawn";
+};
+
+export type ClientEvent = ClientInputEvent | ClientRespawnEvent;
+
+export type ServerGameStateUpdateEvent = {
+  type: "gamestateupdate";
+  gameState: GameState;
+};
+
+export type ServerEvent = ServerGameStateUpdateEvent;
